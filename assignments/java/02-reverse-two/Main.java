a91792a4fe548eeba44b6a08a86f4319b27ad1d0
MayuFujiwara <100046095@csisd.org>
2026-09-10 21:30:06 +0000

    Submit: java/02-reverse-two

--- assignments/java/02-reverse-two/Main.java
+++ assignments/java/02-reverse-two/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Write your solution here.
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        IO.print(b + " " + a);
     }
 }
