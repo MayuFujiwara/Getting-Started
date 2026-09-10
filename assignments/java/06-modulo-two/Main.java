ee4ac69db434bb9b23323fedc319052f7b8217c3
MayuFujiwara <100046095@csisd.org>
2026-09-10 21:50:38 +0000

    Submit: java/06-modulo-two

--- assignments/java/06-modulo-two/Main.java
+++ assignments/java/06-modulo-two/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        IO.print(a%b);
     }
 }
