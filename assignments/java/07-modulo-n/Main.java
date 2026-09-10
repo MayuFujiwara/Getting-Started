1232cfaab9f062e913e42668cd38a42d97cea0d7
MayuFujiwara <100046095@csisd.org>
2026-09-10 22:00:17 +0000

    Submit: java/07-modulo-n

--- assignments/java/07-modulo-n/Main.java
+++ assignments/java/07-modulo-n/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = 0;
+        int c = 0;
+        c = scanner.nextInt();
+        for(int i = 0; i < a - 1; i++){
+            b = scanner.nextInt();
+            c %= b;
+        }
+
+        System.out.print(c);
     }
 }
