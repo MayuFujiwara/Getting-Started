5cadda853c4817a60cb98f8c25de9dd0d982a0e0
MayuFujiwara <100046095@csisd.org>
2026-09-10 21:43:44 +0000

    Submit: java/03-reverse-n

--- assignments/java/03-reverse-n/Main.java
+++ assignments/java/03-reverse-n/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int a = Integer.parseInt(scanner.nextLine());
+        int[] b = new int[a];
+        for(int i = 0; i < a; i++){
+            b[i] = scanner.nextInt();
+        }
+
+        for(int i = a - 1; i >= 0; i--){
+            IO.print(b[i] + " ");
+        }
     }
 }
