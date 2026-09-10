8f07ccbd1028cf2405129b8bc85e26c1f04ad1d3
MayuFujiwara <100046095@csisd.org>
2026-09-10 21:49:35 +0000

    Submit: java/05-sum-n

--- assignments/java/05-sum-n/Main.java
+++ assignments/java/05-sum-n/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int a = scanner.nextInt();
+        long b = 0;
+        for(int i = 0; i < a; i++){
+            b += scanner.nextInt();
+        }
+        IO.print(b);
     }
 }
