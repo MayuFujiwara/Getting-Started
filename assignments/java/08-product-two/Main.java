02c75fa0314f63bda76fc9c1b4481b3affce012f
MayuFujiwara <100046095@csisd.org>
2026-09-10 22:02:07 +0000

    Submit: java/08-product-two

--- assignments/java/08-product-two/Main.java
+++ assignments/java/08-product-two/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        System.out.print((a * b)%1000000007);
     }
 }
