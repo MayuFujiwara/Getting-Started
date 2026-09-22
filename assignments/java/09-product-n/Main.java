8d578a1017f76a55487b4c2cded96ca5e0340854
MayuFujiwara <100046095@csisd.org>
2026-09-22 21:48:17 +0000

    Submit: java/09-product-n

--- assignments/java/09-product-n/Main.java
+++ assignments/java/09-product-n/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int a = Integer.parseInt(scanner.nextLine()); 
+        long b = scanner.nextLong();
+        long c = 0;
+        for(int i = 0; i < a - 1; i++){
+            c = scanner.nextLong();
+            b *= c;
+        }
+        b %= 1000000007;
+
+        IO.print(b);
     }
 }
